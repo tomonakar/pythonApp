58f7f556bf2b27cbcb3f35326c3ac7d6705a22de
tomonakar <tomohisa.nak@gmail.com>
2018-05-27 17:42:10 +0900

    feat: 辞書型のコピー

--- 02_dataStracture/04_dictionary_type.py
+++ 02_dataStracture/04_dictionary_type.py
@@ -20,3 +20,47 @@ print(x)
 
 y = dict([('a', 10), ('b', 20)])
 print(y)
+
+# ----------------------------------------------- #
+# 辞書型のメソッド
+# ----------------------------------------------- #
+d = {'x': 10, 'y': 20}
+# キーのみ取り出す
+print(d.keys())
+
+# バリューのみ取り出す
+print(d.values())
+
+d2 = {'x': 1000, 'j': 500}
+print(d2)
+
+# dをd２でアップデートする
+d.update(d2)
+print(d)
+
+# key 'x'の値を取り出す
+print(d['x'])
+print(d.get('x'))
+
+# 無いキーを取り出すとNoneとなる
+r = d.get('z')
+print(r)
+print(type(r))
+
+# popで値を取り出す
+print(d.pop('x'))
+print(d)
+
+# delでキーバリューを削除
+del d['y']
+print(d)
+
+# clearで中身を空にする
+d = {'a': 100, 'b': 200}
+d.clear()
+print(d)
+
+# キーがあるか判定
+d = {'a': 100, 'b': 200}
+print('a' in d)
+print('c' in d)
