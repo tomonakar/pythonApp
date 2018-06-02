e4c8bd29eb92968089a505ca7c8a4a2b917e6bf1
tomonakar <tomohisa.nak@gmail.com>
2018-06-02 22:05:32 +0900

    feat: 値判定のTips

--- 03_control_flow/02.in_not.py
+++ 03_control_flow/02.in_not.py
@@ -28,3 +28,22 @@ if is_ok:
 
 if not is_ok:
     print('hello hello')
+
+# -------------------------------------------------- #
+# 値が入っているかどうかを判定するTips
+# -------------------------------------------------- #
+
+# is_ok = True
+# 1 は Trueとなる
+is_ok = 1
+# 0 は Falseとなる（0, 0.0, '', (), [] これらすべてFalse
+is_ok = 0
+# 値が入っていればTrueとなる
+is_ok = 'dsafjalsdfjk'
+
+# JSだと変数に値が入っているかどうかを hoge.length > 0 などで判定するが
+# python だとシンプルに判定できる
+if is_ok:
+    print('OK')
+else:
+    print('No')
